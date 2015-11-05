c5378823993fb1158c3cd43c724813f0d79226d6
Sambuddha Basu <sambuddhabasu1@gmail.com>
2015-11-05 13:58:35 +0530

    Templates initialized

--- CloudComputing/chartify.py
+++ CloudComputing/chartify.py
@@ -3,7 +3,7 @@
 
 from flask import Flask
 import os
-from flask import request, redirect, url_for, send_from_directory
+from flask import request, redirect, url_for, send_from_directory, render_template
 from werkzeug import secure_filename
 
 UPLOAD_FOLDER = '/home/NightFury13/CloudComputing/uploads'
@@ -55,28 +55,8 @@ def allowed_file(filename):
 
 @app.route('/dummy')
 def dummy_graph():
-    return '''
-    <!doctype html>
-    <head>
-    <title>Dummy Graph</title>
-        <link href="static/c3/c3.css" rel="stylesheet" type="text/css">
-        <script src="static/c3/d3/d3.min.js" charset="utf-8"></script>
-        <script src="static/c3/c3.min.js"></script>
-    </head>
-    <body>
-        <h1>Dummy Graph in C3.js</h1>
-        <div id="chart"></div>
-        <script>
-            var chart = c3.generate({
-                bindto: '#chart',
-                data: {
-                  columns: [
-                    ['data1', 30, 200, 100, 400, 150, 250],
-                    ['data2', 50, 20, 10, 40, 15, 25]
-                  ]
-                }
-            });
-        </script>
-        <p>Hurray! It works!</p>
-    </body>
-    '''
+    return render_template('dummy.html')
+
+
+if __name__ == '__main__':
+    app.run()
